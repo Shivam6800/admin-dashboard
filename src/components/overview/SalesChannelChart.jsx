82417fc949355dbe5a873849ac0c9d5dc9cc1b0d
Shivam6800 <168638625+Shivam6800@users.noreply.github.com>
2026-04-17 11:16:12 +0530

    Update SalesChannelChart.jsx

--- src/components/overview/SalesChannelChart.jsx
+++ src/components/overview/SalesChannelChart.jsx
@@ -1,3 +1,4 @@
+//sales channel chart component
 import { motion } from "framer-motion";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend, Cell } from "recharts";
 
